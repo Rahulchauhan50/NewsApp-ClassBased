9ee801a5c2413c1b79748bd688e3943ce3813a9c
rahul <rahulchauhah50@gmail.com>
2023-04-26 18:54:34 +0530

    Category added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,76 +1,95 @@
 import React, { Component } from 'react'
 
 export class Navbar extends Component {
+  constructor() {
+    super()
+  }
   render(props) {
-    let {mode, toggle} = this.props;
+    let { mode, toggle } = this.props;
     const styley = {
-      position:'absolute',
-      right:"25%",
-      top:"15px",
+      position: 'absolute',
+      right: "25%",
+      top: "15px",
       cursor: "pointer"
     }
 
     return (
-      <div>
-        {console.log(this.props.mode)}
+      <>
+      <div className='fixed-top'>
         <nav className={`navbar navbar-expand-lg bg-${this.props.mode} navbar-${this.props.mode}`}>
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">QuickNews</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/">Home</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/">Link</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Categories
-                    </a>
-                    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="/">business</a></li>
-                        <li><a className="dropdown-item" href="/">entertainment</a></li>
-                        <li><a className="dropdown-item" href="/">general</a></li>
-                        <li><a className="dropdown-item" href="/">health</a></li>
-                        <li><a className="dropdown-item" href="/">science</a></li>
-                        <li><a className="dropdown-item" href="/">sports</a></li>
-                        <li><a className="dropdown-item" href="/">technology</a></li>
-                    </ul>
-                    </li>
-                    <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Countries
-                    </a>
-                    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="/">business</a></li>
-                        <li><a className="dropdown-item" href="/">entertainment</a></li>
-                        <li><a className="dropdown-item" href="/">general</a></li>
-                        <li><a className="dropdown-item" href="/">health</a></li>
-                        <li><a className="dropdown-item" href="/">science</a></li>
-                        <li><a className="dropdown-item" href="/">sports</a></li>
-                        <li><a className="dropdown-item" href="/">technology</a></li>
-                    </ul>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link disabled" href='/'>Disabled</a>
-                    </li>
-                </ul>
-                <form className="d-flex" role="search">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                </form>
-                </div>
-                <div style={styley} className="form-check form-switch">
-              <input style={{cursor:"pointer"}} onClick={toggle} className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked={mode === "dark"?true:false} />
-                <label id="inp1" className={`text-${mode === "dark"? "light":"dark"}`} style={{fontSize:"13px"}} >Dark Mode</label>
-                </div>
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">QuickNews</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/">Link</a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Categories
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="/">business</a></li>
+                    <li><a className="dropdown-item" href="/">entertainment</a></li>
+                    <li><a className="dropdown-item" href="/">general</a></li>
+                    <li><a className="dropdown-item" href="/">health</a></li>
+                    <li><a className="dropdown-item" href="/">science</a></li>
+                    <li><a className="dropdown-item" href="/">sports</a></li>
+                    <li><a className="dropdown-item" href="/">technology</a></li>
+                  </ul>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Countries
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="/">business</a></li>
+                    <li><a className="dropdown-item" href="/">entertainment</a></li>
+                    <li><a className="dropdown-item" href="/">general</a></li>
+                    <li><a className="dropdown-item" href="/">health</a></li>
+                    <li><a className="dropdown-item" href="/">science</a></li>
+                    <li><a className="dropdown-item" href="/">sports</a></li>
+                    <li><a className="dropdown-item" href="/">technology</a></li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled" href='/'>Disabled</a>
+                </li>
+              </ul>
+              <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success" type="submit">Search</button>
+              </form>
             </div>
+            <div style={styley} className="form-check form-switch">
+              <input style={{ cursor: "pointer" }} onClick={toggle} className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked={mode === "dark" ? true : false} />
+              <label id="inp1" className={`text-${mode === "dark" ? "light" : "dark"}`} style={{ fontSize: "13px" }} >Dark Mode</label>
+            </div>
+          </div>
         </nav>
-      </div>
+        <ul className="overflow-auto list-group list-group-horizontal position-relative overflow-auto w-100">
+          <li style={{borderRadius: "20px",backgroundColor:"#0d6efd",}} className="btn btn-primary text-light my-1 mx-1 py-0 list-group-item">business</li>
+          <li style={{borderRadius: "20px",backgroundColor:"#0d6efd",}} className="btn btn-primary text-light my-1 mx-1 py-0 list-group-item">entertainment</li>
+          <li style={{borderRadius: "20px",backgroundColor:"#0d6efd",}} className="btn btn-primary text-light my-1 mx-1 py-0 list-group-item">general</li>
+          <li style={{borderRadius: "20px",backgroundColor:"#0d6efd",}} className="btn btn-primary text-light my-1 mx-1 py-0 list-group-item">health</li>
+          <li style={{borderRadius: "20px",backgroundColor:"#0d6efd",}} className="btn btn-primary text-light my-1 mx-1 py-0 list-group-item">science</li>
+          <li style={{borderRadius: "20px",backgroundColor:"#0d6efd",}} className="btn btn-primary text-light my-1 mx-1 py-0 list-group-item">sports</li>
+          <li style={{borderRadius: "20px",backgroundColor:"#0d6efd",}} className="btn btn-primary text-light my-1 mx-1 py-0 list-group-item">technology</li>
+    </ul>  
+    </div>
+        <div className="container mt-5">
+        <div style={{height:"15px"}} className="mt-5"></div>
+        <h3 className={`text-${mode==="dark"?"light":"dark"} mt-5`}>Breaking News at Your Fingertips: Download the QuickNews App and Stay Informed Anytime, Anywhere</h3>
+        
+        </div>
+            </>
+
     )
   }
 }
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,25 +7,33 @@ export class News extends Component {
     constructor(){
         super();
         this.state = {
-            articles: [{}],
+            articles: [],
             laoding : true,
-            Category : "general",
+            Category : "sports",
             Country : "in"
         }
     }
     async componentDidMount(){
         this.setState({laoding: true})
-        await fetch(`https://newapi.org/v2/top-headlines?country=${this.state.Country}&category=${this.state.Category}&apiKey=1e0a9b492d664dd1b3751ee483316063&pageSize=6&page=${this.state.articles.page}`)
+        await fetch(`https://newsapi.org/v2/top-headlines?country=${this.state.Country}&category=${this.state.Category}&apiKey=1e0a9b492d664dd1b3751ee483316063&pageSize=6&page=${this.state.articles.page}`)
         .then((data)=>{
             return data.json()
         })
         .then((respone)=>{
-            this.setState({articles: respone.articles, page : 1, results : respone.totalResults-6,laoding: false})
+            this.setState({
+                articles: respone.articles, 
+                page : 1, 
+                results : respone.totalResults-6,
+                laoding: false,
+                totalPages : respone.totalResults%6==0?respone.totalResults/6:((respone.totalResults/6)+1)-((respone.totalResults%6)/6)
+            })
+            
         })
+        console.log(this.state.totalPages)
     }
     HandleOnPrev = async () => {
         this.setState({laoding: true})
-        await fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=1e0a9b492d664dd1b3751ee483316063&pageSize=6&page=${this.state.page-1}`)
+        await fetch(`https://newsapi.org/v2/top-headlines?country=${this.state.Country}&category=${this.state.Category}&apiKey=1e0a9b492d664dd1b3751ee483316063&pageSize=6&page=${this.state.page-1}`)
         .then((data)=>{
             return data.json()
         })
@@ -36,7 +44,7 @@ export class News extends Component {
     HandleOnNext = async () => {
         this.setState({laoding: true})
         console.log(this.state.articles)
-        await fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=1e0a9b492d664dd1b3751ee483316063&pageSize=6&page=${(this.state.page)+1}`)
+        await fetch(`https://newsapi.org/v2/top-headlines?country=${this.state.Country}&category=${this.state.Category}&apiKey=1e0a9b492d664dd1b3751ee483316063&pageSize=6&page=${(this.state.page)+1}`)
         .then((data)=>{
             return data.json()
         })
@@ -44,7 +52,6 @@ export class News extends Component {
             this.setState({articles: respone.articles, page : this.state.page+1, results : this.state.results-6,laoding: false})
             console.log(this.state.results-6)
         })
-
     }
    
   render(props) {
@@ -53,12 +60,11 @@ export class News extends Component {
     return (
         <>
       <div className='container' id='up'>
-        <h3 className={`text-${mode==="dark"?"light":"dark"} my-3`}>Breaking News at Your Fingertips: Download the QuickNews App and Stay Informed Anytime, Anywhere</h3>
         {this.state.laoding && <Spinner/>}
       <div className='row'>
-      {!this.state.laoding && (this.state.articles).map((elements)=>{
-        return <div className={`my-3 col-md-4`} key={elements.url}>
-                <Newsitems mode={mode} title={elements.title} despription={elements.description>130?elements.description.slice(0,130):elements.description} imageurl={elements.urlToImage} newurl={elements.url}/>
+      {!this.state.laoding && (this.state.articles).map(Elements=>{
+        return <div className={`my-3 col-md-4`} key={Elements.url}>
+                <Newsitems mode={mode} title={Elements.title} despription={Elements.description>130?Elements.description.slice(0,130):Elements.description} imageurl={Elements.urlToImage} newurl={Elements.url}/>
             </div>
       })}
       </div>
@@ -66,7 +72,13 @@ export class News extends Component {
       <div className='flex-row d-flex justify-content-center my-3'>
       <div onClick={this.HandleOnPrev} className={`col-md-1 mx-auto btn btn-${mode === "dark"?"dark":"primary"} ${this.state.page<2?"disabled":""}`}><a className='link-light' style={{textDecoration:"none"}} href='#up'>&larr; Previous</a></div>
       
-      <div className='col-4'></div>
+      <div className='col-4'>
+        <div className="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+            <div className="btn-group me-2" role="group" aria-label="First group">
+                <button type="button" className="btn btn-outline-secondary">1</button>
+            </div>
+        </div>
+      </div>
       
       <div onClick={this.HandleOnNext} className={`col-md-1 mx-auto btn btn-${mode === "dark"?"dark":"primary"} ${this.state.results<1?"disabled":""}`} ><a className='link-light' style={{textDecoration:"none"}} href='#up'>Next &rarr;</a></div>
     </div>
